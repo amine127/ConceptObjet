81be0ceabe2610a0bdc4fcd1899e43acc7416a0a
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2019-09-30 14:44:58 +0200

    First test map displaying

---
+++ src/assets/Map.java
@@ -0,0 +1,24 @@
+package assets;
+
+public class Map {
+	private int lengthMap;
+	private int widthMap;
+	
+	public Map(int length, int width) {
+		this.lengthMap = length;
+		this.widthMap = width;
+	}
+	
+	@Override
+	public String toString() {
+		for(int i = 0; i < this.lengthMap; i++) {
+			for (int j = 0; j < this.widthMap; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		return super.toString();
+	}
+	
+	
+}
---
+++ src/our_app/MainApp.java
@@ -0,0 +1,13 @@
+package our_app;
+
+import assets.Map;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Map testMap = new Map(10, 10);
+		System.out.println(testMap);
+		System.out.println("DSDFqsdfQSDFQEdf");
+	}
+
+}
