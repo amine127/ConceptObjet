331e81a7cfdf878d2057f9bdb1be1fe88058655d
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2019-11-06 15:00:41 +0100

    Controlled display for each step of the simulation
    Adding in the MainApp Class of a BufferedReader to allow the user to
    chose between displaying the next step of the simulation or leaving.

--- src/my_app/MainApp.java
+++ src/my_app/MainApp.java
@@ -1,21 +1,40 @@
 package my_app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 import map_assets.Map;
 
 public class MainApp {
 
-	public static void main(String[] args) {
-		Map.getInstance().displayMap();
-		System.out.println();
+	public static void main(String[] args) throws IOException {
 		
-		/*TEST SURROUNDINGS*/
-		//Random randomGenerator = new Random();
-		//int rdmIndex1 = randomGenerator.nextInt(testMap.getNbrLine()), rdmIndex2 = randomGenerator.nextInt(testMap.getNbrColumn());
-		//System.out.println(testMap.getListBox().get(rdmIndex1).get(rdmIndex2));
-		//System.out.println("Surroundings : " + testMap.surroundings(testMap.getListBox().get(rdmIndex1).get(rdmIndex2)));
-		/*TEST SURROUNDINGS*/
-		
+		while (true) {	
+			Map.getInstance().displayMap();
+			System.out.println();
+			
+			/*TEST SURROUNDINGS*/
+			//Random randomGenerator = new Random();
+			//int rdmIndex1 = randomGenerator.nextInt(Map.getInstance().getNbrLine()), rdmIndex2 = randomGenerator.nextInt(Map.getInstance().getNbrColumn());
+			//System.out.println(Map.getInstance().getListBox().get(rdmIndex1).get(rdmIndex2));
+			//System.out.println("Surroundings : " + Map.getInstance().surroundings(Map.getInstance().getListBox().get(rdmIndex1).get(rdmIndex2)));
+			/*TEST SURROUNDINGS*/
+			
+			/*BufferedReader to select to display next step of the simulation or leave.*/
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String line = "";
+			while (line.equalsIgnoreCase("q") == false) {
+				System.out.println("Press Enter for next step or type \"q\" to quit.");
+			    line = in.readLine();
+			}
+			if (line.equalsIgnoreCase("q")) {
+				in.close();
+				System.out.println("You've chosen to leave the simulation, goodbye.");
+			    System.exit(0);
+			}
+		    /*End BufferedReader*/
+		}
 	}
 }
