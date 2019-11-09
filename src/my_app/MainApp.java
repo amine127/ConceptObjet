a1be356911e33a0832d4bb4a87205e3cdda822d5
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2019-11-09 20:55:04 +0100

    Manual/Auto mode 2.0
    In auto mode, .simulation stops at 30 iterations

--- src/my_app/MainApp.java
+++ src/my_app/MainApp.java
@@ -1,34 +1,103 @@
 package my_app;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
+import map_assets.Box;
 import map_assets.Map;
 
 public class MainApp {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
 		
-		Map.getInstance().displayMap();
-		
-		while (Map.getInstance().getListCharacter().size() > 1) {
-			
-			/*BufferedReader to select to display next step of the simulation or leave.*/
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			String line = "";
-			while (line.equalsIgnoreCase("q") == false) {
-				System.out.println("Press Enter for next step or type \"q\" to quit.");
-			    line = in.readLine();
-			    Map.getInstance().newStep();
-			}
-			if (line.equalsIgnoreCase("q")) {
-				in.close();
-				System.out.println("You've chosen to leave the simulation, goodbye.");
-			    System.exit(0);
-			}
-		    /*End BufferedReader*/
+		int count = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Use automatic (a) or manual (m) ?");
+		String choice = scan.next();
+		while (!(choice.equals("a") || choice.equals("m"))) {
+			System.out.println("Wrong answer, please select between automatic(a) and manual(m).");
+			choice = scan.next();
 		}
-		System.out.println("The simulation is over and " + Map.getInstance().getListCharacter().get(0).getContentBox().getClass() + " won the war.");
+		
+		
+		if (choice.equals("a")) {
+			Map.getInstance().displayMap();
+			while (Map.getInstance().getListCharacter().size() > 1 && count != 30) {
+				Map.getInstance().newStep();
+				count++;
+			}
+			
+			if (count < 30) {
+				System.out.println("The simulation is over and " + Map.getInstance().getListCharacter().get(0).getContentBox().getClass() + " won the war.");
+			}
+			else {
+				int nbrGood = 0;
+				int nbrBad = 0;
+				for (Box box : Map.getInstance().getListCharacter()) {
+					if (box.getContentBox().getIsGood()) {
+						nbrGood++;
+					}
+					else {
+						nbrBad++;
+					}
+				}
+				if (nbrGood - nbrBad == 0) {
+					System.out.println("Simulation is over but no winner found.");
+				}
+				if (nbrGood - nbrBad < 0) {
+					System.out.println("Simulation is over and The Bad ones won.");
+				}
+				if (nbrGood - nbrBad > 0) {
+					System.out.println("Simulation is over and the Good ones won.");
+				}
+			}
+			System.exit(0);
+		}
+		
+		
+		else {
+			Map.getInstance().displayMap();
+			while (Map.getInstance().getListCharacter().size() > 1 && count != 30) {
+				System.out.println("Select next step (n) or quit (q).");
+				String next = scan.next();
+				while (!next.equals("q") && !next.equals("n")) {
+					System.out.println("Wrong answer, please select next step (n) or quit (q).");
+					next = scan.next();
+				}
+				if (next.equals("q")) {
+					System.out.println("You've chosen to quit the simulation, goodbye.");
+					System.exit(0);
+				}
+				else {
+					Map.getInstance().newStep();
+					count++;
+				}
+			}
+			
+			if (count < 30) {
+				System.out.println("The simulation is over and " + Map.getInstance().getListCharacter().get(0).getContentBox().getClass() + " won the war.");
+			}
+			else {
+				int nbrGood = 0;
+				int nbrBad = 0;
+				for (Box box : Map.getInstance().getListCharacter()) {
+					if (box.getContentBox().getIsGood()) {
+						nbrGood++;
+					}
+					else {
+						nbrBad++;
+					}
+				}
+				if (nbrGood - nbrBad == 0) {
+					System.out.println("Simulation is over but no winner found.");
+				}
+				if (nbrGood - nbrBad < 0) {
+					System.out.println("Simulation is over and The Bad ones won.");
+				}
+				if (nbrGood - nbrBad > 0) {
+					System.out.println("Simulation is over and the Good ones won.");
+				}
+			}
+			System.exit(0);
+		}
 	}
 }
